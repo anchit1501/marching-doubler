02d81d56fecd75b59435a03f63fdf06261ae43d2
Anchit Aggarwal <1501.anchit@gmail.com>
2022-02-02 01:34:46 +1100

    Task Achieved

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+function generateNumericSet(runSize: number, seriesLength: number) {
+  let maxValue: number = runSize - 1,
+    defaultSetvalue = Array.from({length: maxValue}).map((v, i) => i + 1);
+
+  const list = Array.from({length: Math.ceil(seriesLength / runSize)})
+    .map((runSize, index: number) => {
+      let repeatingNumberIndex = index % maxValue,
+        set = [
+          ...defaultSetvalue.slice(0, repeatingNumberIndex),
+          repeatingNumberIndex + 1,
+          ...defaultSetvalue.slice(repeatingNumberIndex),
+        ];
+      return set;
+    })
+    .flat()
+    .slice(0, seriesLength)
+    .join(',');
+
+  console.log(list);
+}
+
+function marchingDoubler(length: number, terms: number) {
+  if (length && terms && length > 0 && terms > 0) {
+    generateNumericSet(length, terms);
+  } else {
+    return false;
+  }
+  return;
+}
+
+marchingDoubler(4, 2);
